41a683a2e0780c48aa34b6b70fa5f571ddcab10d
Bill Christian <probeilly@gmail.com>
2024-08-12 21:07:26 +0800

    update build

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
-import { ThemeContext } from './contexts/ThemeContext';
-import { Main, BlogPage, ProjectPage } from './pages'
-import { BackToTop } from './components'
-import ScrollToTop from './utils/ScrollToTop'
+import { Main, BlogPage, ProjectPage } from './pages';
+import { BackToTop } from './components';
+import ScrollToTop from './utils/ScrollToTop';
 
-import './App.css'
+import './App.css';
 
 function App() {
-
-  const { theme } = useContext(ThemeContext);
-
   return (
     <div className="app">
       <Router>
-        <ScrollToTop/>
+        <ScrollToTop />
         <Switch>
           <Route path="/" exact component={Main} />
           <Route path="/blog" exact component={BlogPage} />
           <Route path="/projects" exact component={ProjectPage} />
-
           <Redirect to="/" />
         </Switch>
       </Router>
